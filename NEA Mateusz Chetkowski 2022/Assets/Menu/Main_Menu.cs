dcf79226f25df3d3926ebeaad0d31070597cda8d
matusz019 <65133130+matusz019@users.noreply.github.com>
2021-09-17 15:43:08 +0100

    Broken menu

---
+++ NEA Mateusz Chetkowski 2022/Assets/Menu/Main_Menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu : MonoBehaviour {
+
+	public void PlayGame ()
+	{
+		SceneManager.LoadScene(1);
+	}
+	public void SettingMenu ()
+	{
+		Debug.Log ("SETTINGS");
+	}
+
+		
+	public void QuitGame ()
+	{
+		Debug.Log ("QUIT");
+	}
+}
+/*This code gets the active scene inside the build index and then adds one to the build index in order to go to the next scene.
+I figured out how to do this by looking through Unity documentation during my spare time.*/
