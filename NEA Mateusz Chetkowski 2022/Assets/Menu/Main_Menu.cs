5d69309de53c694a69a8f8b49f01647534bd72b8
matusz019 <65133130+matusz019@users.noreply.github.com>
2021-09-21 10:32:09 +0100

    Fixed menu V3

--- NEA Mateusz Chetkowski 2022/Assets/Menu/Main_Menu.cs
+++ NEA Mateusz Chetkowski 2022/Assets/Menu/Main_Menu.cs
@@ -5,24 +5,43 @@ using UnityEngine.SceneManagement;
 
 public class Main_Menu : MonoBehaviour {
 
+	public string Difficulty = "";						//Declaring the diffculty variable
+
+
 	public void PlayGame ()
 	{
-		SceneManager.LoadScene(1);
+		SceneManager.LoadScene(1);						//Loads the game scene
 	}
 	public void SettingMenu ()
 	{
-		SceneManager.LoadScene(2);
+		SceneManager.LoadScene(2);						// Loads the setting menu scene
 	}
 
 	public void BackButton()
 	{
-		SceneManager.LoadScene (0);
+		SceneManager.LoadScene (0);						// Loads the main menu 
+	}
+
+	public void EasyDifficulty ()
+	{
+		Difficulty = "Easy";
+		Debug.Log ("Difficulty = Easy");				// Sets difficulty setting to easy
+	}
+	public void NormalDifficulty ()
+	{
+		Difficulty = "Normal";
+		Debug.Log ("Difficulty = Normal");				//Sets difficulty setting to normal
+	}
+	public void HardDifficulty ()
+	{
+		Difficulty = "Hard";
+		Debug.Log ("Difficulty = Hard");				//Sets difficulty setting to hard
 	}
 
 		
 	public void QuitGame ()
 	{
-		Debug.Log ("QUIT");
+		Debug.Log ("QUIT");								//Exits the game
 	}
 }
 /*This code gets the active scene inside the build index and then adds one to the build index in order to go to the next scene.
