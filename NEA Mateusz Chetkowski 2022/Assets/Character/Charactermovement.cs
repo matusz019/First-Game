21149938ceb9699f1ffe07852241e263adeaf0c9
matusz019 <65133130+matusz019@users.noreply.github.com>
2021-09-24 15:48:34 +0100

    Game Progress V1
    Started making the game

---
+++ NEA Mateusz Chetkowski 2022/Assets/Character/Charactermovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charactermovement : MonoBehaviour {
+	Rigidbody2D rb;
+	float speed;
+
+	// Use this for initialization
+	void Start () {
+		speed = 0.9f;
+		rb = GetComponent<Rigidbody2D> ();	
+	}
+		
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		rb.AddForce (new Vector2 (moveHorizontal, moveVertical) * speed, ForceMode2D.Impulse);
+	}
+}
