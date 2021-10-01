2e78be623e821cfc752fa7df3a3fae9a97ef91d7
matusz019 <65133130+matusz019@users.noreply.github.com>
2021-10-01 12:59:43 +0100

    Game progress V4
    Added turning to my character

---
+++ NEA Mateusz Chetkowski 2022/Assets/Character/Camtracking.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camtracking : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var dir = GameObject.FindGameObjectWithTag ("MainCamera").transform.position;
+		GameObject.FindGameObjectWithTag ("Player").transform.position = dir;
+	}
+}
---
+++ NEA Mateusz Chetkowski 2022/Assets/Character/Mousetourn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mousetourn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var dir = Input.mousePosition - Camera.main.WorldToScreenPoint (transform.position);			//This line gets the vector between the mouse and the game object by subtracting them from eachother
+		var angle = Mathf.Atan2 (dir.y, dir.x) * Mathf.Rad2Deg;					//This line caclulates the angle between the vectors on the plane and then converts them both to degrees
+		transform.rotation = Quaternion.AngleAxis (angle, Vector3.forward);		//This line rotates the sprite to the corresponding angle
+	}
+}
