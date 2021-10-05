0b70ec47718fc3d55943189758af239ed977b8b9
matusz019 <65133130+matusz019@users.noreply.github.com>
2021-10-05 10:58:50 +0100

    Game V5
    Start of sprint 3

---
+++ NEA Mateusz Chetkowski 2022/Assets/Character/Shooting.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * created: Sprint 3
+ * Last Edited: Sprint 3
+ * Purpose: This script instantiates the bullet and adds a velocity to it
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+
+	public GameObject bullet;
+	public Transform firePos;
+	public float bulletSpeed = 20f;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Mouse0))
+		{
+			Shoot();
+		}
+	}
+
+	void Shoot()
+	{
+		GameObject bullet = Instantiate (bullet, firePos.position, firePos.rotation);
+		Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+		rb.AddForce (firePos.up = bulletSpeed, ForceMode2D.Impulse);
+	}
+}
