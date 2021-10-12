9904fb4042d1592ec6e85caf9f877cbd96fc9bee
matusz019 <65133130+matusz019@users.noreply.github.com>
2021-10-12 10:54:51 +0100

    Game V7
    Start of sprint 4

---
+++ NEA Mateusz Chetkowski 2022/Assets/Bullet/SpawnBullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBullet : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+	public Transform firePos;
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+			Instantiate (bulletPrefab, firePos.position, firePos.rotation);
+			Debug.Log ("shoot!");
+		}
+	}
+}
---
+++ NEA Mateusz Chetkowski 2022/Assets/Enemy/enemyMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
