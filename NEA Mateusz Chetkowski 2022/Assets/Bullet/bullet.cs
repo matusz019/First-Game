ae776499cebd3211a6b4ac8bfee8fb6d4c74fd3b
matusz019 <65133130+matusz019@users.noreply.github.com>
2021-10-15 15:49:27 +0100

    Game V8

--- NEA Mateusz Chetkowski 2022/Assets/Bullet/bullet.cs
+++ NEA Mateusz Chetkowski 2022/Assets/Bullet/bullet.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections;
+﻿/*
+ * created: Sprint 3
+ * Last Edited: Sprint 4
+ * Purpose: This script Adds velocity to the bullet at the right position and destroys it upon collision with something
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
--- NEA Mateusz Chetkowski 2022/Assets/Enemy/enemyMove.cs
+++ NEA Mateusz Chetkowski 2022/Assets/Enemy/enemyMove.cs
@@ -1,16 +1,38 @@
-﻿using System.Collections;
+﻿/*
+ * created: Sprint 4
+ * Last Edited: Sprint 4
+ * Purpose: This script manages the movement of my enemies that move back and forth
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class enemyMove : MonoBehaviour {
 
+
+	public Rigidbody2D rb;
+	private float speed = 5f;
+	public bool moveLeft = true;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Update () {
+		if (moveLeft == true) {
+			rb.velocity = transform.up * speed;
+		} else {
+			rb.velocity = transform.up * -speed;
+		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void OnTriggerEnter2D (Collider2D col){
+		if(col.gameObject.tag == "colL"){
+			moveLeft = false;
+			Debug.Log ("I have collided with left");
+		}
+		if (col.gameObject.tag == "colR") {
+			moveLeft = true;
+			Debug.Log ("I have collided with right");
+		}
 	}
 }
