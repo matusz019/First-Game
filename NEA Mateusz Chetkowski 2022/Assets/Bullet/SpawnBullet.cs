3972a25b13d0e9f91747899f1f7a20136036b466
matusz019 <65133130+matusz019@users.noreply.github.com>
2022-01-07 15:48:22 +0000

    V21

--- NEA Mateusz Chetkowski 2022/Assets/Bullet/SpawnBullet.cs
+++ NEA Mateusz Chetkowski 2022/Assets/Bullet/SpawnBullet.cs
@@ -13,14 +13,15 @@ public class SpawnBullet : MonoBehaviour {
 	public GameObject bulletPrefab;
 	public Transform firePos;
 	public bool haveGun = false;
+	public bool brokenGun = false;
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+		if (Input.GetKeyDown (KeyCode.Mouse0) && brokenGun == false) {
 			Instantiate (bulletPrefab, firePos.position, firePos.rotation);
 			//Debug.Log ("shoot!");
 		}
-		if (haveGun == true) {
+		if (haveGun == true && brokenGun == false) {
 			if (Input.GetKey (KeyCode.Mouse0)) {
 				Instantiate (bulletPrefab, firePos.position, firePos.rotation);
 			}
@@ -33,6 +34,11 @@ public class SpawnBullet : MonoBehaviour {
 			StartCoroutine (AutoGun());
 			Debug.Log ("I have picked up gun");
 		}
+		if (col.gameObject.tag == "BrokenGun") {
+			StartCoroutine (BrokenGun());
+			Destroy (col.gameObject);
+			Debug.Log ("I have picked up broken gun");
+		}
 	}
 
 	IEnumerator AutoGun()
@@ -40,5 +46,11 @@ public class SpawnBullet : MonoBehaviour {
 		haveGun = true;
 		yield return new WaitForSeconds (5);
 		haveGun = false;
+	}
+	IEnumerator BrokenGun()
+	{
+		brokenGun = true;
+		yield return new WaitForSeconds (5);
+		brokenGun = false;
 	}
 }
---
+++ NEA Mateusz Chetkowski 2022/Assets/Flashbang.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Flashbang: MonoBehaviour
+
+{
+	public Image myPanel;
+	float fadeTime = 3f;
+	Color colourToFadeTo;
+
+	void Start () {
+		colourToFadeTo = new Color (1f, 1f, 1f, 0f);
+		myPanel.CrossFadeColor (colourToFadeTo, 0.1f, true, true);
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		if (col.gameObject.tag == "Player"){
+			StartCoroutine (Flash());
+
+		}
+	}
+
+	IEnumerator Flash()
+	{
+		Debug.Log ("flashing in progress");
+		colourToFadeTo = new Color (1f, 1f, 1f, 1f);
+		myPanel.CrossFadeColor (colourToFadeTo, 0.1f, true, true);
+
+		yield return new WaitForSeconds (1);
+		Debug.Log ("I have been flashed");
+
+		colourToFadeTo = new Color (1f, 1f, 1f, 0f);
+		myPanel.CrossFadeColor (colourToFadeTo, fadeTime, true, true);
+		Debug.Log ("Flashing done");
+		Destroy (gameObject);
+	}
+
+
+
+}
